d3797003d09e581931c4c70274d7c5a7687546e1
fzc20070415 <fzc20070415@users.noreply.github.com>
2019-02-06 20:04:20 -0800

    Seasnet Update

--- src/main.py
+++ src/main.py
@@ -5,16 +5,6 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-# from util import *
-# from collections import Counter
-#
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.neighbors import KNeighborsClassifier
-# # from sklearn.cross_validation import cross_val_score
-# # from sklearn.cross_validation import train_test_split
-# from sklearn.model_selection import cross_val_score
-# from sklearn.model_selection import train_test_split
-# from sklearn import metrics
 
 ### GLOBAL VARIABLES ###
 # We set risk-free rate at 3.01%. Data obtained from treasury.gov, using Treasury 20-yr CMT
@@ -120,8 +110,8 @@ def estimate_call_put(raw, vol, start=1, end=0):
         # print(d1, d2)
 
         # We ignore divident here
-        N1 = scipy.stats.norm(0, 1).cdf(d1)
-        N2 = scipy.stats.norm(0, 1).cdf(d2)
+        N1 = scipy.stats.norm.cdf(d1)
+        N2 = scipy.stats.norm.cdf(d2)
         call = (N1 * S) - (N2 * K * exp(rate * year_difference))
         N_1 = 1 - N1
         N_2 = 1 - N2
@@ -166,16 +156,6 @@ def check_estimation(est, raw, start=1, end=0):
         else:
             print("Neither C or P detected at i = " + str(i))
 
-    # Plot histograms
-    # TODO
-
-    # Plot Dots vs Linear
-    plt.scatter(est_l, act_l)
-    plt.plot([1],[1])
-    plt.xlabel('estimated data')
-    plt.ylabel('actual data')
-    plt.show()
-
     # Print average absolute difference and percentage difference
     num = 0
     if (end == 0):
@@ -186,6 +166,18 @@ def check_estimation(est, raw, start=1, end=0):
     avg_perc_diff = sum(perc_table)/num
     print("Average absolute error is " + str(avg_abs_diff))
     print("Average percentage error is " + str(avg_perc_diff))
+
+    # Plot histograms
+    # TODO
+
+    # Plot Dots vs Linear
+    # plt.scatter(est_l, act_l)
+    # plt.plot([1],[1])
+    # plt.xlabel('estimated data')
+    # plt.ylabel('actual data')
+    # plt.show()
+
+    return avg_abs_diff, avg_perc_diff
 
 
 def main():
@@ -213,13 +205,52 @@ def main():
     #     print(raw_hp[i])
 
 
-    # Test function
-    volatility = get_volatility(raw_hp)
-    call_put_table = estimate_call_put(raw_data, volatility)
+    # # Test function (All data)
+    # volatility = get_volatility(raw_hp)
+    # call_put_table = estimate_call_put(raw_data, volatility)
+    # # Evaluation
+    # check_estimation(call_put_table, raw_data)
 
-    # Evaluation
-    check_estimation(call_put_table, raw_data)
+    # Use different volatility to estimate call & put
+    len_hp = len(raw_hp)
 
+    report = [['First Period', 'last Period', 'Risk Free Rate', 'Estimated Volatility', 'Avg Abs Err', 'Avg Perc Err']]
+    if 1:
+        for r in range(0, 500, 5):
+            global rate
+            print(r)
+            rate = rate - r*0.00001
+            for i in range(1, len_hp, 20):
+                for j in range(0, len_hp, 20):
+                    k = len_hp - j - 1
+                    if k <= i:
+                        break
+                    if k <2045:
+                        break
+                    # print(i, k)
+                    # Estimate volatility using data from i to k
+                    print("Using rate = " + str(rate))
+                    print("Using data from " + raw_hp[i][0] + " to " + raw_hp[k][0])
+                    volatility = get_volatility(raw_hp, start=i, end=k)
+                    call_put_table = estimate_call_put(raw_data, volatility)
+
+                    avg_abs_err, avg_perc_err = check_estimation(call_put_table, raw_data)
+                    report.append([raw_hp[i][0], raw_hp[k][0], rate, volatility, avg_abs_err, avg_perc_err])
+
+
+    # start=800, end=2200 gives best estimation for volatility for original risk free rate
+
+
+
+    # write into csv
+    with open("../report/report.csv", 'w') as book:
+        wr = csv.writer(book, dialect='excel')
+        for row in report:
+            wr.writerow(row,)
+
+
+
+    print("Task done. Exiting main function.")
 
 if __name__ == "__main__":
     main()
